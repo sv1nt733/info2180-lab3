427a74d9a5c407f7361a3b1fb611618f2e766749
Dillon Waugh <waughdillon66@gmail.com>
2021-10-11 00:19:43 -0400

    User click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,32 @@
 document.addEventListener('DOMContentLoaded', ()  => {
-  const gameBoard = document.getElementById("board").children;
+    const gameBoard = document.getElementById("board").children;
+    const xButton= "X";
+    const oButton="O";
+    let clicked;
+    let play=true;
+    var i;
+    var gameState=['','','','','','','','',''];
+  
+    for(i=0;i<gameBoard.length;i++){
+      gameBoard[i].setAttribute("class","square");
+      gameBoard[i].addEventListener('click',clicks);
+    }
+    
+    function clicks(event) {
+      console.log(gameState);
+      if(play==true || clicked==oButton){
+        event.target.innerHTML= xButton;
+        event.target.classList.add("X");
+        clicked= xButton;
+        play=false;
+      }
+      else if(clicked==xButton || play==false){
+        event.target.innerHTML=oButton;
+        event.target.classList.add("O");
+        clicked=oButton;
+        play=true;
+      }  
+      }
+    })
+  
 
-  for(i=0;i<gameBoard.length;i++){
-    gameBoard[i].setAttribute("class","square");
-  }
-})
